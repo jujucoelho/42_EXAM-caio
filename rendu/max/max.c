3fbad8b189533be07d14a482fea984294a0f6f8b
JujuCoelho <juliacoelhobrandao@gmail.com>
2025-04-03 20:42:53 +0000

    teste

--- rendu/max/max.c
+++ rendu/max/max.c
@@ -5,7 +5,6 @@ int	max(int *tab, unsigned int len)
 	
 	max = tab[0];
 	i = len - 1;
-	printf("i = %d, max = %i, tab[i] = %d\n", i, max, tab[i]);
 	if (len == 0)
 		return (0);
 	else
@@ -14,10 +13,8 @@ int	max(int *tab, unsigned int len)
 		{
 			if (max < tab[i])
 				max = tab[i];
-			printf("2)i = %d, max = %i, tab[i] = %d\n", i, max, tab[i]);
 			i--;
 		}
-		printf("3)i = %d, max = %i, tab[i] = %d\n", i, max, tab[i]);
 		return (max);
 	}
 }
