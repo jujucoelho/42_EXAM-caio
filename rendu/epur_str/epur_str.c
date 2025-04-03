85d42feda12654beb987492d4ee09b4c7d5fe005
JujuCoelho <juliacoelhobrandao@gmail.com>
2025-04-03 22:09:12 +0000

    ju

--- rendu/epur_str/epur_str.c
+++ rendu/epur_str/epur_str.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -12,18 +11,15 @@ int	main(int argc, char **argv)
 		{
 			if (argv[1][i] > 32)
 			{
-				//printf("Imprime letras %c\n",argv[1][i]);
 				write(1, &argv[1][i], 1);
 				if ((argv[1][i + 1] <= 32) && (argv[1][i + 2]))
 				{
 					write(1, " ", 1);
-				//	printf("Imprime espaços %c\n",argv[1][i]);
 				}
 			}
 			i++;
 		}
 	}
 	write(1, "\n", 1);
-	printf("this time it will be more complex .$\n");
 	return (0);
 }
