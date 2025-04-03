cddfad1f7f842bd7e6e065d1e5786810a94ef901
JujuCoelho <juliacoelhobrandao@gmail.com>
2025-04-03 19:37:06 +0000

    teste

--- rendu/ft_strcpy/ft_strcpy.c
+++ rendu/ft_strcpy/ft_strcpy.c
@@ -5,7 +5,7 @@
 char	*ft_strcpy(char *s1, char *s2)
 {
 	int	i;
-
+    
 	i = 0;
 	while (s2[i])
 	{
@@ -14,14 +14,4 @@ char	*ft_strcpy(char *s1, char *s2)
 	}
 	s1[i] = '\0';
 	return (s1);
-}
-
-int	main(void)
-{
-	char	s1[50];
-	char	*s2;
-
-	s2 = "Olá mundo";
-	printf("%s", ft_strcpy(s1, s2));
-	return (0);
 }
